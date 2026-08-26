7969f53058daf42a87c6b2747d9aa138cddaa9f0
Nikola Marković <62554959+marckonick@users.noreply.github.com>
2026-08-26 16:15:30 +0200

    Add files via upload

---
+++ Arduino Codes/vent_leak_full_logmel_arduino_nano_33_INT8_ready/saved_features.h
@@ -0,0 +1,12 @@
+#ifndef SAVED_FEATURES_H
+#define SAVED_FEATURES_H
+
+// Declare the array and its size
+extern double dataArray[];
+extern const int dataArraySize;
+
+extern const double dataMean[];
+extern const double dataStd[];
+
+
+#endif
